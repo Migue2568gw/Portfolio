846b339da1d5591dfd0f266f1af8f77303a373f3
Migue2568gw <migue2000gw@gmail.com>
2023-06-30 12:15:46 -0500

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Bio } from '../biografia/Bio';
+import Portafolio from '../portafolio/Portafolio';
+import Header from '../header/Header'
+import { NavBar } from '../navBar/NavBar'
+
+
+export const App = () => {
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home localitation="#Home"  />}/>
+        <Route path="/biografia" element={<Bio localitation="#Bio" />} />
+        <Route path="/portafolio" element={<Portafolio localitation="#portfolio"/>} />
+      </Routes>
+    </Router>
+  );
+};
+
+const Home = ({ localitation }) => {
+  const initialNav = NavBar({ localitation });
+  const initialNavRef = useRef(null);
+  initialNavRef.current = initialNav;
+  return 
+};
+
+export default App;
